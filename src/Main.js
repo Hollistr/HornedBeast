537336726aa5cc31af58df157af66c678ab49843
Chris Hollis <chrisbhollis@gmail.com>
2022-10-19 18:00:30 -0700

    edits to App/HornedBeast/Main.js

--- src/Main.js
+++ src/Main.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import HornedBeast from './HornedBeast.js';
+import Row from 'react-bootstrap/Row';
 // import onehorned from './one-horned.jpg';
 // import twohorned from './two-horned.jpg';
 // import threehorned from './three-horned.jpg';
@@ -12,19 +13,22 @@ class Main extends React.Component {
             <p className='instructions'>Click to vote on favorite beast</p>
             <p className='instructions'>Click beast for larger image</p>
             <div className='d-flex flexwrap justify-content-center'>
+                <Row xs={1} md={3} className='g-4'>
                 {this.props.beastsToDisplay.map(beast => {
                     return (
                     <HornedBeast
                         title={beast.title}
-                        imgUrl={beast.image_url}
-                        discription={beast.discription}
+                        image_url={beast.image_url}
+                        description={beast.description}
                         key={beast._id}
                         id={beast.id}
                         rating=""
                         setShowModal={this.props.setShowModal}
+                        selectTheBeast={this.props.selectTheBeast}
                     />
                     );
                 })};
+                </Row>
             </div>
             </>
         )}};
